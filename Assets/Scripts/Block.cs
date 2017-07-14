8116c019d79dc4562931686e4bfe66f76ba7f950
Yifan Zhu <zhu_yifan@me.com>
2017-07-14 15:54:21 +0800

    Init

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+    public string[] block;
+    public GameObject blockCube;
+    private float halfSize;
+    private float halfSizeFloat;
+    private float childSize;
+    private bool[,] blockMatrix;
+    private int size;
+
+    // Use this for initialization
+    void Start()
+    {
+        size = block.Length;
+        int width = block[0].Length;
+        halfSize = (size + 1) * 0.5f;
+        childSize = (size - 1) * 0.5f;
+        halfSizeFloat = size * 0.5f;
+        blockMatrix = new bool[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                if (block[y][x] == '1')
+                {
+                    blockMatrix[y, x] = true;
+                    var cube = Instantiate(blockCube, new Vector3(x - childSize, childSize - y, 0), Quaternion.identity) as GameObject;
+                    cube.transform.parent = this.transform;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TetrisSceneManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct FieldBlock
+{
+    public Vector2 blockPosition;
+    public bool isFull;
+    public GameObject fieldBlockPrefab;
+
+    public FieldBlock(Vector2 blockPostion, bool isFull, GameObject fieldBlockPrefab)
+    {
+        this.blockPosition = blockPostion;
+        this.isFull = isFull;
+        this.fieldBlockPrefab = fieldBlockPrefab;
+        this.fieldBlockPrefab.transform.position = this.blockPosition;
+    }
+}
+
+public class TetrisSceneManager : MonoBehaviour
+{
+
+    public int FieldWidth = 10;
+    public int FieldLenght = 20;
+    public FieldBlock[,] field;
+    public GameObject blockPrefab;
+
+    private void Awake()
+    {
+        blockPrefab = Resources.Load<GameObject>("Block");
+    }
+    // Use this for initialization
+    void Start()
+    {
+        CreateTetrisScene();
+        IsFullBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateTetrisScene()
+    {
+        field = new FieldBlock[FieldWidth, FieldLenght];
+        for (int y = 0; y < FieldWidth - 1; y++)
+        {
+            for (int x = 0; x < FieldLenght - 1; x++)
+            {
+                field[y, x] = new FieldBlock(new Vector2(y, x), true, Instantiate(blockPrefab,this.gameObject.transform,false)as GameObject);
+            }
+        }
+    }
+    void IsFullBlock()
+    {
+        for (int y = 0; y < FieldWidth - 1; y++)
+        {
+            for (int x = 0; x < FieldLenght - 1; x++)
+            {
+                if (field[y, x].isFull)
+                {
+                    GameObject obj= GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    obj.transform.parent = field[y, x].fieldBlockPrefab.transform;
+                    obj.transform.position = field[y, x].blockPosition;
+                    obj.transform.localScale = new Vector3(0.99f, 0.99f, 0.99f);
+                }
+            }
+        }
+    }
+}
